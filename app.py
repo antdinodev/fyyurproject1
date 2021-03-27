b8c7b9fb8fb79d410834b77908a303ea634ca0bb
NinjaBob <bobmarketing5000@gmail.com>
2021-03-27 00:25:53 -0700

    new env

--- app.py
+++ app.py
@@ -50,6 +50,8 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+db.create_all()
+
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
